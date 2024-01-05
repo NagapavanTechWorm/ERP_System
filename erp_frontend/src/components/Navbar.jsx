a6ae967a7244bd5531408231be70b88b81d4e648
NagapavanTechWorm <nagapavan.aofficail@gmail.com>
2024-01-05 10:34:38 +0530

    import daisyui

---
+++ erp_frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='bg-base-300'>
+        <div className='navbar align-elements'>
+        <div className='navbar-start'>
+            <h1 className='font-semibold text-4xl'>AIEMS</h1>
+        </div>
+        <div className='navbar-center'></div>
+        <div className='navbar-end'></div>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar;
