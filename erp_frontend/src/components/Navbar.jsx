55d65f9c427fa563f2c35f5491fdd0142e3be086
NagapavanTechWorm <nagapavan.aofficail@gmail.com>
2024-01-05 21:32:02 +0530

    changed little styling

--- erp_frontend/src/components/Navbar.jsx
+++ erp_frontend/src/components/Navbar.jsx
@@ -13,12 +13,12 @@ const Navbar = () => {
 
   return ( 
     <nav className='bg-base-300'>
-        <div className='navbar align-elements p-4'>
+        <div className='navbar align-elements p-1 md:p-4'>
         <div className='navbar-start'>
-            <h1 className='font-bold text-4xl'>AIEMS</h1>
+            <h1 className='font-bold text-2xl md:text-4xl'>AIEMS</h1>
         </div>
         <div className='navbar-center'>
-            <div className='text-5xl font-semibold'>{timer ? <Timer duration={duration} />:
+            <div className='text-2xl  md:text-5xl font-semibold'>{timer ? <Timer duration={duration} />:
             <div className="flex gap-5"> 
     <div>
       <span className="countdown font-mono ">
@@ -36,7 +36,7 @@ const Navbar = () => {
     }</div>
         </div>
         <div className='navbar-end'>
-            <button className='btn btn-error font-semibold text-xl' onClick={handleTimer}>{timer?"Stop/Reset":"Close Door"}</button>
+            <button className='btn btn-error font-semibold text-sm md:text-xl md:py-1' onClick={handleTimer}>{timer?"Stop/Reset":"Close Door"}</button>
         </div>
     </div>
     </nav>
