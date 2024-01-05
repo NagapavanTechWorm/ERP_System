05cbb0c221fc9dd371eae1574ef990d7f5dc04c0
NagapavanTechWorm <nagapavan.aofficail@gmail.com>
2024-01-05 12:21:59 +0530

    fixed timer css

--- erp_frontend/src/components/Navbar.jsx
+++ erp_frontend/src/components/Navbar.jsx
@@ -6,6 +6,11 @@ const Navbar = () => {
     const handleTimer = ()=>{
         setTimer(!timer);
     }
+
+      const duration = 5*60; //input time
+        const minutes = Math.floor(duration/60);
+        const seconds = duration % 60;
+
   return ( 
     <nav className='bg-base-300'>
         <div className='navbar align-elements p-4'>
@@ -13,10 +18,25 @@ const Navbar = () => {
             <h1 className='font-bold text-4xl'>AIEMS</h1>
         </div>
         <div className='navbar-center'>
-            <p className='text-5xl font-semibold'>{timer ? <Timer duration={2 * 60} />:"Timer"}</p>
+            <p className='text-5xl font-semibold'>{timer ? <Timer duration={duration} />:
+            <div className="flex gap-5"> 
+    <div>
+      <span className="countdown font-mono ">
+        <span style={{"--value":minutes}}></span>
+      </span>
+      min
+    </div> 
+    <div>
+      <span className="countdown font-mono ">
+        <span style={{"--value":seconds}}></span>
+      </span>
+      sec
+    </div>
+  </div>
+    }</p>
         </div>
         <div className='navbar-end'>
-            <button className='btn btn-error font-semibold text-xl' onClick={handleTimer}>{timer?"Stop":"Close Door"}</button>
+            <button className='btn btn-error font-semibold text-xl' onClick={handleTimer}>{timer?"Stop/Reset":"Close Door"}</button>
         </div>
     </div>
     </nav>
